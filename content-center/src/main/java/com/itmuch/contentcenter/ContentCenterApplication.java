0c553ae0570af0de076b9b4114f3a53b3bf3c685
3hpyj <3hpyj@3h.net>
2024-04-18 10:43:10 +0800

    feat: feign基础调用

--- content-center/src/main/java/com/itmuch/contentcenter/ContentCenterApplication.java
+++ content-center/src/main/java/com/itmuch/contentcenter/ContentCenterApplication.java
@@ -3,6 +3,7 @@ package com.itmuch.contentcenter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import tk.mybatis.spring.annotation.MapperScan;
@@ -11,6 +12,7 @@ import java.util.Collections;
 
 @MapperScan("com.itmuch.contentcenter.dao")
 @SpringBootApplication
+@EnableFeignClients// (defaultConfiguration = GlobalFeignConfiguration.class)
 public class ContentCenterApplication {
 
     public static void main(String[] args) {
--- content-center/src/main/java/com/itmuch/contentcenter/service/content/ShareService.java
+++ content-center/src/main/java/com/itmuch/contentcenter/service/content/ShareService.java
@@ -4,6 +4,7 @@ import com.itmuch.contentcenter.dao.share.ShareMapper;
 import com.itmuch.contentcenter.domain.dto.content.ShareDTO;
 import com.itmuch.contentcenter.domain.dto.user.UserDTO;
 import com.itmuch.contentcenter.domain.entity.share.Share;
+import com.itmuch.contentcenter.feignclient.UserCenterFeignClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -16,7 +17,7 @@ import org.springframework.stereotype.Service;
 public class ShareService {
 
     private final ShareMapper shareMapper;
-//    private final UserCenterFeignClient userCenterFeignClient;
+    private final UserCenterFeignClient userCenterFeignClient;
 
     public ShareDTO findById(Integer id) {
         // 获取分享详情
@@ -28,12 +29,12 @@ public class ShareService {
         // 2. 复杂的url难以维护：https://user-center/s?ie={ie}&f={f}&rsv_bp=1&rsv_idx=1&tn=baidu&wd=a&rsv_pq=c86459bd002cfbaa&rsv_t=edb19hb%2BvO%2BTySu8dtmbl%2F9dCK%2FIgdyUX%2BxuFYuE0G08aHH5FkeP3n3BXxw&rqlang=cn&rsv_enter=1&rsv_sug3=1&rsv_sug2=0&inputT=611&rsv_sug4=611
         // 3. 难以相应需求的变化，变化很没有幸福感
         // 4. 编程体验不统一
-//        UserDTO userDTO = this.userCenterFeignClient.findById(userId);
+        UserDTO userDTO = this.userCenterFeignClient.findById(userId);
 
         ShareDTO shareDTO = new ShareDTO();
         // 消息的装配
         BeanUtils.copyProperties(share, shareDTO);
-//        shareDTO.setWxNickname(userDTO.getWxNickname());
+        shareDTO.setWxNickname(userDTO.getWxNickname());
         return shareDTO;
     }
 }
